a68b02be217023470b9f01dc3043f895ce28cd92
Sibichakaravarthi-S <sibichakaravarthi588@gmail.com>
2025-06-28 15:07:35 +0530

    Add files via upload

---
+++ Valid Parentheses.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<int> mystack;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i]=='('||s[i]=='{'||s[i]=='[')
+                mystack.push(s[i]);
+            else if((!mystack.empty()) && ((s[i]==')' && mystack.top()=='(')||(s[i]=='}' && mystack.top()=='{')||(s[i]==']' && mystack.top()=='[')))
+                mystack.pop();              
+            else 
+                return false;
+            
+        }
+        return mystack.empty()?true:false;
+    }
+};
