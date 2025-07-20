cad281dc7f4cf5301e0bff91af1ebefdf59b65b9
Sibichakaravarthi-S <sibichakaravarthi588@gmail.com>
2025-07-20 23:44:26 +0530

    Add files via upload

---
+++ How Many Numbers Are Smaller Than the Current Number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+               vector<int> count(101, 0);  // Since 0 <= nums[i] <= 100
+        
+        // Count frequency of each number
+        for(int num : nums) {
+            count[num]++;
+        }
+        
+        // Convert to cumulative count (prefix sum)
+        for(int i = 1; i < 101; i++) {
+            count[i] += count[i-1];
+        }
+        
+        vector<int> result(nums.size());
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] == 0) {
+                result[i] = 0;
+            } else {
+                result[i] = count[nums[i] - 1];
+            }
+        }
+        return result;
+    }
+};
