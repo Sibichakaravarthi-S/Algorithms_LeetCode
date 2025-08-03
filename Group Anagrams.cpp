7fd5c0542a5b467c618acb47ac733e09965cc142
Sibichakaravarthi-S <sibichakaravarthi588@gmail.com>
2025-08-03 18:46:40 +0530

    Add files via upload

---
+++ Group Anagrams.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        
+        unordered_map<string,vector<string>> group;
+
+        for(string& str:strs){
+            string key=str;
+            sort(key.begin(),key.end());
+            group[key].push_back(str);
+        }
+
+        vector<vector<string>> result;
+        for(auto& pair:group){
+            result.push_back(pair.second);
+        }
+        return result;
+    }
+};
